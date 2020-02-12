7dca04a05d528b583341845b75c5b0f150fdc1a8
WirteR <kravetsillis16@gmail.com>
2020-02-12 23:10:14 +0200

    main site section

--- index.js
+++ index.js
@@ -6,3 +6,14 @@
         element.setAttribute("style", `left: -${width - parentWidth}px`);
       }, false);
 }
+
+{
+    imgElement = document.querySelectorAll('.product-img');
+    height1 = document.querySelectorAll('.product-name');
+    height2 = document.querySelectorAll('.product-price');
+    for (var x = 0; x < imgElement.length; x++) {
+      parentHeiht = imgElement[x].parentElement.offsetHeight;
+      imgElement[x].setAttribute("style", `height: ${parentHeiht - (
+          height1[x].offsetHeight + height2[x].offsetHeight) - 22}px`);
+    }   
+}
