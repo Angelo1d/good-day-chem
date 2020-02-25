d1f4950f9725f12391290d0a109b3ee55a20d38e
WirteR <kravetsillis16@gmail.com>
2020-02-25 22:28:46 +0200

    gallery

---
+++ js/item.js
@@ -0,0 +1,54 @@
+clearClass = list => {
+    for (var element of list) {
+        element.classList.remove("active-img");
+    };
+}
+
+{
+    imgChilds = document.querySelector(".gallery-items").children;
+    for (var element of imgChilds) {
+        element.addEventListener('click', function () {
+            clearClass(imgChilds);
+            refreshImg = document.getElementById("selected-img")
+            refreshImg.src = this.src;
+            this.classList.add("active-img");
+        })
+    }
+}
+
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
